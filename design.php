678a89843b774b02b983a102bfeebc0a0bf48f93
Santi25 <santiagoag25@gmail.com>
2017-06-07 23:36:36 +0200

    Add files via upload

---
+++ design.php
@@ -0,0 +1,93 @@
+<!doctype html>
+<html><head>
+<meta charset='utf-8'>
+<title>Escandallos </title>
+<meta name='viewport' content='width=device-width, initial-scale=1'>
+<link rel='stylesheet' href='css/bootstrap.css'>
+<link href='starter-template.css' rel='stylesheet'>
+<link rel='stylesheet' href='css/main.css'>
+<link rel='stylesheet' href='css/jumbotron.css'>
+ 
+</head>
+ 
+ <body>
+ 	
+ <h1></h1>
+     <div class='container'>
+     	
+        <div class='row'>
+         <nav class='navbar navbar-fixed-top navbar-inverse ' role='navigation'>
+        	<div class='navbar-header'>
+          		<button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#navbar' aria-expanded='false' aria-controls='navbar'>
+           			<span class='sr-only'>Toggle navigation</span>
+            		<span class='icon-bar'></span>
+            		<span class='icon-bar'></span>
+            		<span class='icon-bar'></span>
+         		 </button>
+                &nbsp;&nbsp;&nbsp; <img alt='Brand' src='images/inicio.png'>&nbsp;&nbsp;&nbsp;
+          		
+       		</div>
+        	<div id='navbar' class='collapse navbar-collapse'>
+          		<ul class='nav navbar-nav'>
+            		<li class='active'><a href='index.php'>&nbsp;&nbsp;&nbsp;<b>Inicio</b>&nbsp;&nbsp;&nbsp;</a></li>
+            		<li><a href='#'>&nbsp;&nbsp;&nbsp;<b>Escandallos</b>&nbsp;&nbsp;&nbsp;</a></li>
+                    <li><a href='#'>&nbsp;&nbsp;&nbsp;<b>Registrate</b>&nbsp;&nbsp;&nbsp;</a></li>
+            		
+                    
+          		</ul>
+                 <form class='navbar-form navbar-right' role='form'>
+            <div class='form-group has-warning'>
+              <input type='text' placeholder='Email' class='form-control'>
+            </div>
+            <div class='form-group has-warning'>
+              <input type='password' placeholder='Contraseña' class='form-control'>
+            </div>
+            <button type='submit' class='btn btn-primary'>Iniciar sesion</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          </form>
+        </div>
+        	</div><!--/.nav-collapse -->
+            </nav>
+                    
+         <div class='jumbotron'>
+      <div class='container '>
+        <h1>Hello, world!</h1>
+        <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+        <p><a class='btn btn-primary btn-lg' href='#' role='button'>Learn more &raquo;</a></p>
+      </div>
+    </div>
+<p class="bg-danger">Donec ullamcorper nulla non metus auctor fringilla.</p> 
+    <div class='container centro'>
+      <!-- Example row of columns -->
+      <div class='row'>
+        <div class='col-md-4'>
+          <h2>Heading</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class='btn btn-default' href='#' role='button'>View details &raquo;</a></p>
+        </div>
+        <div class='col-md-4'>
+          <h2>Heading</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class='btn btn-default' href='#' role='button'>View details &raquo;</a></p>
+       </div>
+        <div class='col-md-4'>
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          <p><a class='btn btn-default' href='#' role='button'>View details &raquo;</a></p>
+        </div>
+      </div>
+
+      <hr>
+
+      <footer>
+        <p>&copy; Company 2014</p>
+      </footer>
+    </div>
+    
+
+     <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script>
+
+	<script src='js/jquery.min.js'></script>
+	<script src='js/bootstrap.js'></script>
+    
+</body>
+</html>
